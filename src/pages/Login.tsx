8a27fe99392b323756dbd4ac75f17c3ebbed2cac
My Event Advisor <158420635+liledask@users.noreply.github.com>
2025-05-21 09:34:13 -0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,9 +29,9 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to our messaging system to connect
-          </h2>
+          <h3 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            The messaging space for event people
+          </h3>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
